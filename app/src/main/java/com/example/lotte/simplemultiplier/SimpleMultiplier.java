f596496e354f20e9a0876727da14433cad4886f9
LotteIshoey <lija12@student.aau.dk>
2016-02-29 21:35:54 +0100

    Added comments to the java functions.

--- app/src/main/java/com/example/lotte/simplemultiplier/SimpleMultiplier.java
+++ app/src/main/java/com/example/lotte/simplemultiplier/SimpleMultiplier.java
@@ -10,20 +10,21 @@ public class SimpleMultiplier extends AppCompatActivity {
     public int number = 0;
     public int result_nbr = 0;
 
+    // Decrements and displays the number when "-" button is pressed.
     public void decrement(View view){
         number = number - 1;
-
         TextView displaydecrement = (TextView) findViewById(R.id.show_number);
         displaydecrement.setText(" " + number);
-
     }
+
+    // Increments and displays the number when "+" button is pressed.
     public void increment(View view){
         number = number + 1;
-
         TextView displayincrement = (TextView) findViewById(R.id.show_number);
         displayincrement.setText(" " + number);
     }
 
+    // Multiplies the number by 3 and displays the result.
     public void calculate(View view){
         result_nbr = number * 3;
         TextView myTextViewcal = (TextView) findViewById(R.id.show_result);
